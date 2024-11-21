b33c4cc8e66824b91846f403c774053d49cec215
ADITYA <kartikmobile2244@gmail.com>
2024-11-21 15:54:27 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ADITYA</div>
+          <div className="right">
+            <p>MIT muzaffarpur, Muzaffarpur</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ADITYA</p>
+          </div>
+          <div className="right">
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
